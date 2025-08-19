beda049ad49b96293de5d4f6295452c16e812192
Nafees <nafees6388@gmail.com>
2025-08-19 15:36:15 +0530

    Demoo.java

--- Demoo.java
+++ Demoo.java
@@ -1,10 +1,18 @@
 package example;
 
+import java.util.Scanner;
+
 public class Demoo {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		String st1, st2;
+		System.out.println("string");
+		System.out.println("Enter the first string (string 1)");
+		st1 = scan.nextLine();
+		System.out.println("Enter the second string (string 2)");
+		st2 = scan.nextLine();
+		System.out.println(st1 + "\n" + st2);
 	}
-
 }
+
+
